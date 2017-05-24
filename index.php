4de604f09a8f614d94da5aa526606114bb71920b
Aleksandra Stoszko <astoszko@zssalezjanie.com>
2017-05-24 12:05:45 +0200

    Połączenie z bazą danych

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'Articles';
 
-
+$connect = mysql_connect($dbhost,$dbuser,$dbpass);
+mysql_select_db($dbname,$connect);
 
 
 
