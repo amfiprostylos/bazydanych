dcb61d2694bfe38c80269bd4b6843786e6bc4433
Aleksandra Stoszko <astoszko@zssalezjanie.com>
2017-06-06 11:56:21 +0200

    Dodany autor i content + zmiana w method

--- index.php
+++ index.php
@@ -7,9 +7,17 @@ $dbname = 'Articles';
 $connect = mysql_connect($dbhost,$dbuser,$dbpass);
 mysql_select_db($dbname,$connect);
 
+if(isset($_GET['author']) and isset($_GET['content']))
+{
+
+$author = $_GET['author'];
+$content = $_GET['content'];
+
+mysql_query("INSERT INTO Article VALUES (null,'$author','$content'')");
+}
 ?>
 
-<form action="GET">
+<form method="GET">
     <input type="text" name="author">
     <input type="text" name="content">
     <input type="submit" value="dodaj do bazy danych">
